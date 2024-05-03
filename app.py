533014e97041cacbd18e0ac55b16e8a0976dc095
Dzkmobw <zhangxuanzhao233@gmail.com>
2024-05-03 16:52:34 +0800

    添加了age 增加了player类的crud

---
+++ app.py
@@ -0,0 +1,120 @@
+from flask import request, json
+from flask import Flask
+from flask.views import MethodView
+from extension import db,cors
+from models import Player
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:mysqlPassw0rd@localhost:3306/testdb?charset=utf8mb4'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = False
+db.init_app(app)
+cors.init_app(app)
+
+#终端输入flask create即可初始化数据库
+@app.cli.command() # 自定义指令
+def create():
+    db.drop_all()   # 把旧的数据表全部删除
+    db.create_all() # 创建一个新的数据表
+    Player.init_db()  # 初始化数据
+
+class PlayerApi(MethodView):
+    def get(self,player_id):
+        #获取图书信息
+        # 如果没有指定ID，返回所有球员
+        if not player_id:
+            players: [Player] = Player.query.all() # 类型注释，表示Players是一个列表，列表中的元素都是Player元素
+            results = [
+                {
+                    'id':player.id,
+                    'player_name':player.player_name,
+                    'player_number':player.player_number,
+                    'player_position':player.player_position,
+                    'goals':player.goals,
+                    'assists':player.assists,
+                    'age':player.player_age,
+                } for player in players
+            ] # 列表推导式
+            ret = {
+                'status' : 'success',
+                'message' : '数据查询成功',
+                'results': results
+            }
+        # 指定了ID，返回单本书
+        else:
+            player: Player = Player.query.get(player_id)
+            ret= {
+                'status': 'success',
+                'message': '数据查询成功',
+                'results': {
+                    'id':player.id,
+                    'player_name':player.player_name,
+                    'player_number':player.player_number,
+                    'player_position':player.player_position,
+                    'goals':player.goals,
+                    'assists':player.assists,
+                    'age':player.player_age,
+
+                }
+            }
+        # 返回中文
+        return json.dumps(ret, ensure_ascii=False)
+
+    def post(self):
+        #新增球员
+        form = request.json
+        player = Player()
+        player.player_name = form.get('player_name')
+        player.player_number = form.get('player_number')
+        player.player_position = form.get('player_position')
+        player.goals = form.get('goals')
+        player.assists = form.get('assists')
+        player.player_age = form.get('player_age')
+        db.session.add(player)
+        db.session.commit()
+        ret =  {
+            'status': 'success',
+            'message': '数据添加成功',
+        }
+        return json.dumps(ret, ensure_ascii=False)
+
+    def delete(self,player_id):
+        #删除球员
+        player = Player.query.get(player_id)
+        db.session.delete(player)
+        db.session.commit()
+        ret = {
+            'status': 'success',
+            'message': '数据删除成功',
+        }
+        return json.dumps(ret, ensure_ascii=False)
+
+    def put(self,player_id):
+        #数据库更新
+        player:Player = Player.query.get(player_id)
+        player.player_name = request.json.get('player_name')
+        player.player_number = request.json.get('player_number')
+        player.player_position = request.json.get('player_position')
+        player.goals = request.json.get('goals')
+        player.assists = request.json.get('assists')
+        player.player_age = request.json.get('player_age')
+        db.session.commit()
+        ret =  {
+            'status': 'success',
+            'message': '数据修改成功',
+        }
+        return json.dumps(ret, ensure_ascii=False)
+
+player_view = PlayerApi.as_view('player_api')
+# 注意斜杠问题
+app.add_url_rule('/books/',
+                 defaults={'player_id':None},
+                 view_func=player_view,methods=['GET',])
+app.add_url_rule('/books/',
+                 view_func=player_view,methods=['POST',])
+app.add_url_rule('/books/<int:player_id>',
+                 view_func=player_view,
+                 methods=['GET','PUT','DELETE'])
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- models.py
+++ models.py
@@ -5,26 +5,25 @@ class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement = True)
     player_number = db.Column(db.String(255),nullable = False)
     player_name = db.Column(db.String(255),nullable = False)
+    player_age = db.Column(db.String(255))
     player_position = db.Column(db.String(255),nullable = False)
     goals = db.Column(db.String(255))
     assists = db.Column(db.String(255))
 
-    @staticmethod
-    def init_db():
-        '''
-        进行数据库的初始化
-        :return:
-        '''
-        rets = [
-            (1,'21','安东尼·马特乌斯·多斯·桑托斯','RW',114,514),
+        #初始化数据库
+@staticmethod
+def init_db():
+    rets = [
+        (1,'21','安东尼·马特乌斯·多斯·桑托斯',21,'LeftBack',114,514),
         ]
-        for ret in rets:
-            player = Player()
-            player.id = ret[0]
-            player.player_number = ret[1]
-            player.player_name = ret[2]
-            player.player_position = ret[3]
-            player.goals = ret[4]
-            player.assists = ret[5]
-            db.session.add(player)
-        db.session.commit()
+    for ret in rets:
+        player = Player()
+        player.id = ret[0]
+        player.player_number = ret[1]
+        player.player_name = ret[2]
+        player.player_age = ret[3]
+        player.player_position = ret[4]
+        player.goals = ret[5]
+        player.assists = ret[6]
+        db.session.add(player)
+    db.session.commit()
